b7efa773c5012f04ec2ce55d807c87cec0da1ba3
정상원 <89632518+CodeHotel@users.noreply.github.com>
2023-11-30 12:42:56 +0900

    map update

--- Cluster/Cluster/MainPage.xaml.cs
+++ Cluster/Cluster/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Maps;
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 namespace Cluster
 {
     public partial class MainPage : ContentPage
@@ -9,6 +10,38 @@ namespace Cluster
         {
             InitializeComponent();
         }
+
+        public void AddMarker(double latitude, double longitude)
+        {
+            var pin = new Pin
+            {
+                Label = $"{count++}",
+                Type = PinType.Place,
+                Location = new Location(latitude, longitude)
+            };
+            MyMap.Pins.Add(pin);
+        }
+
+        public (double Latitude, double Longitude) GetViewportCenter()
+        {
+            var center = MyMap.VisibleRegion?.Center;
+            if (center == null) return (0, 0);
+            return (center.Latitude, center.Longitude);
+        }
+
+        private void onv1(object sender, EventArgs e)
+        {
+            AddMarker(GetViewportCenter().Latitude, GetViewportCenter().Longitude);
+        }
+
+        private void OnMapTapped(object sender, EventArgs e)
+        {
+            if (sender is Microsoft.Maui.Controls.Maps.Map)
+            {
+                // Handle map tap event
+            }
+        }
+
 
     }
 
